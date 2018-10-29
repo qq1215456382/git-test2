420316451bd615b313490521297852985bcc05a8
mawei <1215456382@qq.com>
2018-10-29 19:00:21 +0800

    修改，添加

--- src/com/itheima/gitText.java
+++ src/com/itheima/gitText.java
@@ -5,5 +5,9 @@ public class gitText {
     public static void main(String[] args) {
         System.out.println("hahaah ");
         System.out.println("asdadasd");
+        System.out.println("asdadasd");
+        System.out.println("asdadasd");
+
+
     }
 }
